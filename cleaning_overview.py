2677ac73e03a50aafe76c5b1e12e3089579b94bd
svtlys <alyssagomez123456@gmail.com>
2026-03-18 14:28:25 -0700

    Recommiting File

--- cleaning_overview.py
+++ cleaning_overview.py
@@ -10,10 +10,9 @@ df = pd.read_csv(file_path)
 # This file is intended to understand the dataset data and clean data
 # Note that there are no nulls and that the dataset is already quite clean 
 
-# ----------------------------
+# -----------------------------
 # Basic Overview
-# ----------------------------
-
+# -----------------------------
 print("\nFirst 5 rows:")
 print(df.head())
 
@@ -26,9 +25,9 @@ print(df.columns)
 print("\nData types:")
 print(df.dtypes)
 
-# ----------------------------
+# -----------------------------
 # Check for null values 
-# ----------------------------
+# -----------------------------
 
 # Note that this dataset doesn't have any null values 
 print("\nMissing values:")
@@ -36,9 +35,9 @@ print(df.isnull().sum())
 
 
 
-# ----------------------------
+# -----------------------------
 # 4. Quick Statistical Summary
-# ----------------------------
+# -----------------------------
 
 print("\nSummary statistics (numeric columns):")
 print(df.describe())
