550b0ae03dd983daf0ed6a711b8425649843c94e
Aywei2001 <aywei2013@gmail.com>
2026-05-03 10:43:44 -0700

    Udated baseline model script to 80/20 with edited models, results CSV file is removed due to size being too large

--- baseline/baseline_enhanced_model.py
+++ baseline/baseline_enhanced_model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.linear_model import LinearRegression
 import xgboost as xgb
 import numpy as np
-from sklearn.metrics import mean_squared_error, accuracy_score, r2_score
+from sklearn.metrics import mean_squared_error, accuracy_score, r2_score, mean_absolute_error
 import joblib
 
 df = pd.read_csv('stratified_sample.csv')
@@ -30,20 +30,14 @@ X = pd.get_dummies(X, columns=['satellite', 'instrument', 'type', 'version', 'co
 
 X['acq_time'] = pd.to_numeric(X['acq_time'], errors='coerce').fillna(0)
 
-#split 10% for testing 70% for training 20% for validation
-X_temp, X_test_lr, y_temp, y_test_lr = train_test_split(X, y, test_size=0.1, random_state=42)
-
-#take  about 22% to get 20% of the original total for validation
-X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.222, random_state=42)
+#split 20% for testing and 80% for training 
+X_train, X_test_lr, y_train, y_test_lr = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #create the model
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 
 joblib.dump(lr_model, 'lr_model.joblib')
-
-#predictions made for validation
-val_predictions = lr_model.predict(X_val)
 
 #predictions made for testing
 lr_predictions = lr_model.predict(X_test_lr)
@@ -52,6 +46,7 @@ lr_predictions = lr_model.predict(X_test_lr)
 lr_r2 = r2_score(y_test_lr, lr_predictions)
 lr_mse = mean_squared_error(y_test_lr, lr_predictions)
 lr_rmse = np.sqrt(lr_mse)
+lr_mae = mean_absolute_error(y_test_lr, lr_predictions)
 
 
 #xgboost model
@@ -72,17 +67,15 @@ X['acq_time'] = pd.to_numeric(X['acq_time'], errors='coerce').fillna(0)
 for col in ['satellite', 'instrument', 'daynight', 'type', 'version', 'confidence']:
     X[col] = X[col].astype('category').cat.codes
 
-#split off the 10% Test set
-#X_remainder will hold the 90% intended for Train + Validation
-X_remainder, X_test_xgb, y_remainder, y_test_xgb = train_test_split(X, y, test_size=0.10, random_state=42)
+#split off 20% for testing
+X_train_full, X_test_xgb, y_train_full, y_test_xgb = train_test_split(X, y, test_size=0.20, random_state=42)
 
-#split the remainder into 70% Train and 20% Validation
-#test_size here is 0.22 because 0.20 / 0.90 is roughly 0.22
-X_train, X_val, y_train, y_val = train_test_split(X_remainder, y_remainder, test_size=0.22, random_state=42)
+#split the remainder into 80% for training
+X_train, X_val, y_train, y_val = train_test_split(X_train_full, y_train_full, test_size=0.10, random_state=42)
 
 #train with early stopping
 #pass the validation set to eval_set so XGBoost can monitor performance
-xgb_model = xgb.XGBRegressor(n_estimators=1000, learning_rate=0.05)
+xgb_model = xgb.XGBRegressor(n_estimators=1000, learning_rate=0.05, early_stopping_rounds=50)
 
 xgb_model.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=False )
 
@@ -96,11 +89,10 @@ xgb_predictions = xgb_model.predict(X_test_xgb)
 xgb_r2 = r2_score(y_test_xgb, xgb_predictions)
 xgb_mse = mean_squared_error(y_test_xgb, xgb_predictions)
 xgb_rmse = np.sqrt(xgb_mse)
+xgb_mae = mean_absolute_error(y_test_xgb, xgb_predictions)
 
 
 #gradient boost model
-#convert daynight to 1 and 0 so the model can read it
-df['daynight'] = df['daynight'].map({'D': 1, 'N': 0})
 
 #drop acq_date and acq_time as dates need special handling
 features = ['latitude', 'longitude', 'brightness', 'scan', 'track', 'acq_date', 'acq_time', 'satellite', 'instrument', 'confidence', 'version', 'bright_t31', 'daynight', 'type']
@@ -113,12 +105,14 @@ X['acq_date'] = (X['acq_date'] - X['acq_date'].min()).dt.days
 
 X['acq_time'] = pd.to_numeric(X['acq_time'], errors='coerce').fillna(0)
 
-for col in X.select_dtypes(include=['object']).columns:
+for col in X.select_dtypes(include=['object', 'string']).columns:
     X[col] = X[col].astype('category').cat.codes
 
-#split into 70% for training 20% for validation 10% 
-X_temp, X_test_gb, y_temp, y_test_gb = train_test_split(X, y, test_size=0.10, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.222, random_state=42)
+#split off 20% for testing
+X_train_full, X_test_gb, y_train_full, y_test_gb = train_test_split(X, y, test_size=0.20, random_state=42)
+
+#split the remainder into 80% for training
+X_train, X_val, y_train, y_val = train_test_split(X_train_full, y_train_full, test_size=0.10, random_state=42)
 
 #train the model
 gb_model = xgb.XGBRegressor(n_estimators=1000, learning_rate=0.05, max_depth=6, early_stopping_rounds=15)
@@ -135,6 +129,7 @@ gb_predictions = gb_model.predict(X_test_gb)
 gb_r2 = r2_score(y_test_gb, gb_predictions)
 gb_mse = mean_squared_error(y_test_gb, gb_predictions)
 gb_rmse = np.sqrt(gb_mse)
+gb_mae = mean_absolute_error(y_test_gb, gb_predictions)
 
 
 #gather everything for CSV file
@@ -145,6 +140,7 @@ final_df = pd.DataFrame({
     'lr_frp_difference': y_test_lr.values - lr_predictions,
     'lr_R2': lr_r2,
     'lr_RMSE': lr_rmse,
+    'lr_MAE': lr_mae,
     
     #XGBoost columns
     'xgb_frp': y_test_xgb.values,
@@ -152,13 +148,15 @@ final_df = pd.DataFrame({
     'xgb_frp_difference': y_test_xgb.values - xgb_predictions,
     'xgb_R2': xgb_r2,
     'xgb_RMSE': xgb_rmse,
+    'xgb_MAE': xgb_mae,
     
     #Gradient Boost columns
     'gb_frp': y_test_gb.values,
     'gb_predict_frp': gb_predictions,
     'gb_frp_difference': y_test_gb.values - gb_predictions,
     'gb_R2': gb_r2,
-    'gb_RMSE': gb_rmse
+    'gb_RMSE': gb_rmse,
+    'gb_MAE': gb_mae
 })
 
 # Save to CSV
